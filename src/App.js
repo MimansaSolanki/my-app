1731888c118e16d4f83dc98b55626b532a0720bc
MimansaSolanki <mimansasolanki605@gmail.com>
2021-10-21 22:30:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
-import data from './data';
-import Display from './Display';
-import React from 'react';
+import React,{ Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import { useState, useEffect } from 'react';
+import Axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [hasMore, setHasmore] = useState(true);
+  useEffect(() => (getItems()), []);
+
+  const options = {
+    method: 'GET',
+    headers: { 
+      X_COUNTRY: "AE",
+      X_VEHICLE_TYPE: "CAR"
+    },
+  };  
+  const getItems = async (page) => {
+      Axios.get(`https://listing-service.qac24svc.dev/v1/vehicle?sf=city:DU_DUBAI&size=25&spath=buy-used-cars-dubai&variant=filterV3&page=${page}`,options)
+      .then((response) => {
+        setHasmore(false);
+        setItems([...items, ...response.data.results]);
+    });
+  }
+
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand " href="https://consumer-web-ae.qac24svc.dev/ae"><img className="logo" src="https://static.cars24.com/cars24/images/cars-logo.png?v=1" alt="logo" /></a>
+          <a className="navbar-brand " href="https://consumer-web-ae.qac24svc.dev/ae"><img className="logo" src="https://consumer-web-ae.qac24svc.dev/ae/static/js/42a884476ab8ee4a3642ef07389f1a63.svg" alt="logo" /></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -31,61 +51,56 @@ function App() {
       </form>
       <h1 className="qf_dm">Used Cars In Dubai</h1>
       <div className="col-sm-3 rounded">
-    {data.map((object, index) => (
-      <Display
-        key={index}
-        index={index}
-        imgUrl = {data[index].mainImage.path}
-        distTravelled = {data[index].odometerReading}
-        carName = {data[index].make}
-      />
-  ))}
-    </div>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={getItems}
+          hasMore={true || false}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {items.map((item) => {
+            return <div className="col-sm-3 rounded">
+              <div className="cars m-2">
+                <div className="image">
+                  <img className="_3oX3Z img-fluid rounded" src={`https://fastly-production.24c.in/${item.mainImage.path}`} />
+                </div>
+
+                <div className="title">
+                  <img className="heart" src="https://consumer-web-ae.qac24svc.dev/ae/static/js/e8333986534ba8a423ce86582be0003d.svg" />
+                  <div className="_3TSwN">
+                    <h2 className="RZ4T7">{item.year} {item.make} {item.model}</h2>
+                    <p className="_1i1E6">{item.variant} |<span>  GCC Specifications</span></p>
+                  </div>
+                  <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/15bbe7d4f9e51f5043535857c52d7336.svg" />
+                  <span> {item.odometerReading} km</span>
+                  <div className="cyl">
+                    <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/5570637da94bead30f0c65f9faca1c5e.svg" />
+                    <span>{item.noOfCylinders}cyl{item.engineSize}L </span>
+                  </div>
+                  <hr />
+                </div>
+
+                <div className="info">
+                  <div className="_3aEWp">
+                    <div className="_3SXwZ"><strong>AED {item.emiDetails.emi}<span>/month</span></strong></div>
+                    <div className="aApXW cyl"><span>AED {item.price}</span></div></div>
+                  <br /> 
+                  <span className="_2xBAf">{item.emiDetails.downPayment} Downpayment</span>
+                  <span className="_2xBAg cyl">AED {item.discountAmount}</span>
+                </div>
+              </div>
+            </div>
+            })
+          }
+        </InfiniteScroll>
+      </div>
     </div>
 
   );
 }
-const container = document.querySelector(".cars");
-let limit = 5;
-let pageCount = 1;
-let postCount = 1;
 
-/**const getPost = async () => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}$_page=${pageCount}$`)
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
-
-  data.map((curElm, index) => {
-    const htmldata = `
-    
-    `
-  }
-  )
-}
-
-getPost();*/
 export default App;
-
-
-
-/**<div class = "cars">
-      <div class = "image">
-        <img className="_3oX3Z img-fluid rounded" src="https://fastly-production.24c.in/inspection-car/appointments/spincar/qa/AE/AE_CAR_9714804977/POST_REFURB/1629815477986/cu-2.jpg?w=397&amp;auto=format" alt="2018 Hyundai Accent GL 1.6 L" />
-      </div>
-
-      <div class = "title">
-        <img className = "heart" src="https://consumer-web-ae.qac24svc.dev/ae/static/js/e8333986534ba8a423ce86582be0003d.svg" alt="Add to wishlist"/>
-        <div className="_3TSwN">
-          <h2 className="RZ4T7">2018 Hyundai Accent</h2>
-          <p className="_1i1E6">GL 1.6 L |<span>  80,862 km</span></p>
-        </div>
-        <hr/>
-      </div>
-
-      <div class = "info">
-        <div className="_3aEWp">
-          <div className="_3SXwZ"><strong>AED 482<span>/month</span></strong></div><div className="aApXW"><span>AED 30,137</span></div></div>
-        <span className="_2xBAf">AED 6,027 downpayment</span>
-      </div>
-    </div> */
