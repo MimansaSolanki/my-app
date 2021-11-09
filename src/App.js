df79d47b79efd8d2e8d130300ec0d4a471d81d1a
MimansaSolanki <92751779+MimansaSolanki@users.noreply.github.com>
2021-11-09 16:08:40 +0530

    Search Box Filter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
-import React,{ Component } from 'react';
+import React from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
 import { useState, useEffect } from 'react';
 import Axios from "axios";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function App() {
   const [items, setItems] = useState([]);
   const [hasMore, setHasmore] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
   useEffect(() => (getItems()), []);
 
   const options = {
     method: 'GET',
-    headers: { 
+    headers: {
       X_COUNTRY: "AE",
       X_VEHICLE_TYPE: "CAR"
     },
-  };  
+  };
   const getItems = async (page) => {
-      Axios.get(`https://listing-service.qac24svc.dev/v1/vehicle?sf=city:DU_DUBAI&size=25&spath=buy-used-cars-dubai&variant=filterV3&page=${page}`,options)
+    Axios.get(`https://listing-service.qac24svc.dev/v1/vehicle?sf=city:DU_DUBAI&size=25&spath=buy-used-cars-dubai&variant=filterV3&page=${page}`, options)
       .then((response) => {
         setHasmore(false);
         setItems([...items, ...response.data.results]);
-    });
+      });
   }
 
   return (
@@ -47,57 +53,70 @@ function App() {
       </nav>
       <div className="container1"><div className="_1_rE8"><nav aria-label="breadcrumb"><ul className="breadcrumb"><li className="breadcrumb-item"><a>Home</a></li><li className="breadcrumb-item">Used Cars in Dubai</li></ul></nav></div></div>
       <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Find your dream car with us" aria-label="Search" />
+        <input className="form-control me-2" type="text" placeholder="Find your dream car with us" aria-label="Search" 
+          onChange = {event => {setSearchTerm(event.target.value)}}
+        />
       </form>
       <h1 className="qf_dm">Used Cars In Dubai</h1>
-      <div className="col-sm-3 rounded">
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={getItems}
-          hasMore={true || false}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={getItems}
+        hasMore={true || false}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {items.filter((item) => {
+          if (searchTerm == "") {
+            return item
+          } else if (item.make.toLowerCase().includes(searchTerm.toLowerCase()) || 
+                     item.model.toLowerCase().includes(searchTerm.toLowerCase()) ) {
+            return item
           }
-        >
-          {items.map((item) => {
-            return <div className="col-sm-3 rounded">
-              <div className="cars m-2">
-                <div className="image">
-                  <img className="_3oX3Z img-fluid rounded" src={`https://fastly-production.24c.in/${item.mainImage.path}`} />
-                </div>
-
-                <div className="title">
-                  <img className="heart" src="https://consumer-web-ae.qac24svc.dev/ae/static/js/e8333986534ba8a423ce86582be0003d.svg" />
-                  <div className="_3TSwN">
-                    <h2 className="RZ4T7">{item.year} {item.make} {item.model}</h2>
-                    <p className="_1i1E6">{item.variant} |<span>  GCC Specifications</span></p>
-                  </div>
-                  <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/15bbe7d4f9e51f5043535857c52d7336.svg" />
-                  <span> {item.odometerReading} km</span>
-                  <div className="cyl">
-                    <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/5570637da94bead30f0c65f9faca1c5e.svg" />
-                    <span>{item.noOfCylinders}cyl{item.engineSize}L </span>
-                  </div>
-                  <hr />
-                </div>
-
-                <div className="info">
-                  <div className="_3aEWp">
-                    <div className="_3SXwZ"><strong>AED {item.emiDetails.emi}<span>/month</span></strong></div>
-                    <div className="aApXW cyl"><span>AED {item.price}</span></div></div>
-                  <br /> 
-                  <span className="_2xBAf">{item.emiDetails.downPayment} Downpayment</span>
-                  <span className="_2xBAg cyl">AED {item.discountAmount}</span>
-                </div>
-              </div>
-            </div>
-            })
-          }
-        </InfiniteScroll>
-      </div>
+        }).map((item) => {
+          return <div className="cars">
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={`https://fastly-production.24c.in/${item.mainImage.path}`}
+                />
+                <CardContent>
+                  <Typography className="heart">
+                    <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/e8333986534ba8a423ce86582be0003d.svg" />
+                  </Typography>
+                  <Typography gutterBottom>
+                    <h4>{item.year} {item.make} {item.model}</h4>
+                    <h6><b>{item.variant}</b> |<span>  GCC Specifications</span></h6>
+                    <br/>
+                  </Typography>
+                  
+                  <Typography>
+                    <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/15bbe7d4f9e51f5043535857c52d7336.svg" />
+                    <span> {item.odometerReading} km</span>
+                    <div className="cyl">
+                      <img src="https://consumer-web-ae.qac24svc.dev/ae/static/js/5570637da94bead30f0c65f9faca1c5e.svg" />
+                      <span>{item.noOfCylinders}cyl{item.engineSize}L </span>
+                    </div>
+                    <hr />
+                    <div className="_3aEWp">
+                      <div className="_3SXwZ"><strong>AED {item.emiDetails.emi}<span>/month</span></strong></div>
+                      <div className="aApXW cyl"><span>AED {item.price}</span></div>
+                    </div>
+                    <br />
+                    <span className="_2xBAf">{item.emiDetails.downPayment} Downpayment</span>
+                    <span className="_2xBAg cyl">AED {item.discountAmount}</span>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </div>
+        })
+        }
+      </InfiniteScroll>
     </div>
 
   );
